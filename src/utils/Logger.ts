48028021383c709421b424080ff4b308889e29f8
IvanRodriCalleja <ivanrodricalleja@gmail.com>
2025-11-20 19:33:12 +0100

    chore: log into ts language server

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,32 +1,13 @@
 import * as ts from 'typescript/lib/tsserverlibrary';
-import fs from 'fs';
-import path from 'path';
 
-const pluginDir = __dirname;
-const debugLogPath = path.join(pluginDir, '../../debug.log');
 export interface Logger {
 	log(msg: string): void;
 }
 
 export class LoggerImpl implements Logger {
-	constructor(private readonly info: ts.server.PluginCreateInfo) {
-		// Clear the debug log on init
-		try {
-			fs.writeFileSync(debugLogPath, `=== Plugin Initialized ${new Date().toISOString()} ===\n`);
-		} catch {
-			// Ignore
-		}
-	}
+	constructor(private readonly info: ts.server.PluginCreateInfo) {}
 
-	public log(message: string) {
-		const timestamp = new Date().toISOString();
-		const logMessage = `[${timestamp}] ${message}\n`;
-
-		// Write to debug file
-		try {
-			fs.appendFileSync(debugLogPath, logMessage);
-		} catch {
-			// Ignore file write errors
-		}
+	public log(msg: string) {
+		this.info.project.projectService.logger.info(`[tailwind-typescript-plugin] ${msg}`);
 	}
 }
